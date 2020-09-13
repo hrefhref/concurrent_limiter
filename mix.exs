7cfb7fc5351460eda202f575d26bb8770f8501cd
Jordan Bracco <href@random.sh>
2020-09-13 12:03:20 +0200

    Release 0.1.1

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule ConcurrentLimiter.MixProject do
     [
       app: :concurrent_limiter,
       description: "A concurrency limiter",
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
